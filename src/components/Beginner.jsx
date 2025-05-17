f8a3c76f5954d4d65ea08554401255f96c3aeb22
farhanhubofficial <shaciye0757@gmail.com>
2025-05-17 21:22:22 +0300

    m

--- src/components/Beginner.jsx
+++ src/components/Beginner.jsx
@@ -257,7 +257,7 @@ function Beginner() {
       </div>
 
       {/* Make the table scrollable on smaller screens */}
-      <div className=" mb-6">
+      {/* <div className=" mb-6">
         <table className="w-full table-auto border">
           <thead>
             <tr>
@@ -291,7 +291,7 @@ function Beginner() {
             ))}
           </tbody>
         </table>
-      </div>
+      </div> */}
 
       {/* Classroom Student Details Modal */}
       {showStudentDetails && selectedClassroom && (
