df3cb74f6e03d5000c4c47f03511c47a844e38c5
Farhan Ali <shaciye0757@gmail.com>
2025-09-16 12:38:31 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Make sure assets and routes resolve correctly on Vercel
+  base: './',  // 🔥 use relative paths so assets load correctly
   server: {
     host: true,
     allowedHosts: [
