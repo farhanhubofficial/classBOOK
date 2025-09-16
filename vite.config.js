0c17510e72ba6eaaf60776ded9d0906aa60f5205
Farhan Ali <shaciye0757@gmail.com>
2025-09-16 12:54:33 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '', // let Vercel serve from root
+  base: '', // ✅ important: empty string so assets resolve correctly
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-});
+})
