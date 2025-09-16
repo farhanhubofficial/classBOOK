c23ba8e47f0f050a5dd2a0cb07c3a25e9d3a5d2b
Farhan Ali <shaciye0757@gmail.com>
2025-09-16 14:40:37 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // ✅ important: empty string so assets resolve correctly
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+    outDir: 'dist'
   },
+  base: '/'
 })
