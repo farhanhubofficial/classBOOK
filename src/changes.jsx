dcec2f2777c7ded91223cebcbc0a18d5ba04b7d8
Farhan Ali <shaciye0757@gmail.com>
2025-09-16 12:51:34 +0300

    m

---
+++ src/changes.jsx
@@ -0,0 +1,35 @@
+// {
+//   "buildCommand": "npm run build",
+//   "outputDirectory": "dist",
+//   "framework": "vite",
+//   "rewrites": [
+//     {
+//       "source": "/(.*)",
+//       "destination": "/index.html"
+//     }
+//   ]
+// }
+// ///verecl
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '',  // 🔥 use relative paths so assets load correctly
+//   server: {
+//     host: true,
+//     allowedHosts: [
+//       'localhost',
+//       'd6ad-41-209-10-50.ngrok-free.app'
+//     ],
+//   },
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//   },
+// })
+// //////
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: './',  // 🔥 use relative paths so assets load correctly
-  server: {
-    host: true,
-    allowedHosts: [
-      'localhost',
-      'd6ad-41-209-10-50.ngrok-free.app'
-    ],
-  },
+  base: '', // let Vercel serve from root
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-})
+});
