b63f9a010d891a5c500b754da81661f04249ea5a
Farhan Ali <shaciye0757@gmail.com>
2025-09-16 18:42:19 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
+ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  base: '',  // 🔥 use relative paths so assets load correctly
+  server: {
+    host: true,
+    allowedHosts: [
+      'localhost',
+      'd6ad-41-209-10-50.ngrok-free.app'
+    ],
   },
-  base: '/'
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
