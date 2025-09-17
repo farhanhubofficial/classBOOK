a664a1b746237a92a46ab9f9032dd2cd251cacbc
Farhan Ali <shaciye0757@gmail.com>
2025-09-17 19:06:55 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // ✅ always use absolute paths for Vercel
+  base: './',   
   build: {
     outDir: 'dist',
     emptyOutDir: true, // ✅ deletes old files so no stale hashes remain
